2503fdb3d61042d5bfdc336dc91571b69be1a99a
Patasnijak <133394047+Patasnijak@users.noreply.github.com>
2025-12-22 21:45:59 +0100

    Update control.py

--- cogs/control.py
+++ cogs/control.py
@@ -4,5 +4,10 @@ class Control(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-async def setup(bot):
-    await bot.add_cog(Control(bot))
+    @commands.slash_command(name="ping", description="Test command for bot")
+    async def ping(self, ctx):
+        await ctx.respond("Pong! ✅")
+
+def setup(bot):
+    bot.add_cog(Control(bot))
+
