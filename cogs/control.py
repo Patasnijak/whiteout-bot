297ba42226b99f3146a6945845537289f1dacc35
Patasnijak <133394047+Patasnijak@users.noreply.github.com>
2025-12-22 21:53:13 +0100

    Update control.py

--- cogs/control.py
+++ cogs/control.py
@@ -5,16 +5,14 @@ class Control(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @app_commands.command(name="restart_bot", description="Startet den Bot neu")
-    async def restart_bot(self, interaction):
-        await interaction.response.send_message("Bot wird neu gestartet...")
-        await self.bot.close()  # Schließt den Bot, Hauptscript kann Neustart handhaben
-
-    @app_commands.command(name="ping", description="Prüft die Latenz des Bots")
+    @app_commands.command(name="ping", description="Zeigt die Latenz des Bots an")
     async def ping(self, interaction):
         await interaction.response.send_message(f"Pong! Latenz: {round(self.bot.latency*1000)}ms")
 
+    @app_commands.command(name="restart", description="Startet den Bot neu")
+    async def restart(self, interaction):
+        await interaction.response.send_message("Bot wird neu gestartet...")
+        await self.bot.close()
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(Control(bot))
-
-
